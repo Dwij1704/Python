56fe571684b57bbb8cb680badb9159aa96ff1bf9
Dwij Patel <dwijpatel1704@gmail.com>
2023-03-23 09:58:16 +0530

    Update

--- IPE/30) Pager.py
+++ IPE/30) Pager.py
@@ -1,2 +1,11 @@
-def pager():
-    
+def pager(filename):
+        f=open(filename,"r")
+        count=0
+        for i in f.readlines():
+            print(i)
+            count+=1
+            if count%25==0:
+                inp=input("Enter continue to read more")
+                if inp=="continue":
+                    break
+pager("Dwij.txt")
